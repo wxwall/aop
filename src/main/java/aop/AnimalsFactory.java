20beeb2b3a2ca28729c88d562cf01d103df79a65
xiaowei.wu <xiaowei.wu@Ctrip.com>
2018-12-29 11:52:02 +0800

    日了狗222

--- src/main/java/aop/AnimalsFactory.java
+++ src/main/java/aop/AnimalsFactory.java
@@ -12,7 +12,7 @@ public class AnimalsFactory {
                              Class<?>[] interfaces,
                              InvocationHandler h){
         System.out.println("");
-        System.out.println("日了狗11111111111");
+        System.out.println("日了狗222222222");
         return (T)Proxy.newProxyInstance(loader,interfaces,h);
     }
 }
